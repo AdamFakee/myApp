3a3669994e6a65c7e07e84b931a9d61ffa4df18c
AdamFakee <buidinhtuan04@gmail.com>
2025-01-01 15:35:36 +0700

    add file index.js, become file root

--- src/route/index.route.js
+++ src/route/index.route.js
@@ -1,5 +1,7 @@
 import { AuthStackGroup } from "./auth.route";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { BottomBarGroup } from "./bottomTab.route";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import {
@@ -11,10 +13,12 @@ export function  IndexStackGroup () {
   return (
     <GestureHandlerRootView className='flex-1'>
       <BottomSheetModalProvider>
-        <IndexStack.Navigator initialRouteName='TabBar' >
-            <IndexStack.Screen name='TabBar' component={BottomBarGroup} options={{headerShown:false}}></IndexStack.Screen> 
-            <IndexStack.Screen name='Auth' component={AuthStackGroup} options={{headerShown:false}}></IndexStack.Screen> 
-        </IndexStack.Navigator>
+        <NavigationContainer>
+          <IndexStack.Navigator initialRouteName='TabBar' >
+              <IndexStack.Screen name='TabBar' component={BottomBarGroup} options={{headerShown:false}}></IndexStack.Screen> 
+              <IndexStack.Screen name='Auth' component={AuthStackGroup} options={{headerShown:false}}></IndexStack.Screen> 
+          </IndexStack.Navigator>
+        </NavigationContainer>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   )
